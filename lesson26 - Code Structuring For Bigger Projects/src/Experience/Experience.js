22f67f28113a5aa5a4b5ba4c4294b4d08f1da93b
Felipe Ballesteros <felipeballesteros@msn.com>
2022-03-18 08:42:00 -0400

    Continuing lesson 26 - 2

--- lesson26 - Code Structuring For Bigger Projects/src/Experience/Experience.js
+++ lesson26 - Code Structuring For Bigger Projects/src/Experience/Experience.js
@@ -5,12 +5,13 @@ import Time from '../Utils/Time'
 import Camera from './Camera'
 import Renderer from './Renderer'
 import World from './World/World'
+import Resources from '../Utils/Resources'
+import sources from './sources'
 
 
 let instance = null
 export default class Experience {
     constructor( canvas ) {
-
 
         // The next two lines allow for creation of singleton
         if(instance) return instance
@@ -27,6 +28,7 @@ export default class Experience {
         this.sizes = new Sizes()
         this.time = new Time()
         this.scene = new THREE.Scene()
+        this.resources = new Resources(sources)
         this.camera = new Camera()
         this.renderer = new Renderer()
         this.world = new World()
---
+++ lesson26 - Code Structuring For Bigger Projects/src/Experience/sources.js
@@ -0,0 +1,14 @@
+export default [
+  {
+    name: 'environmetMapTexture',
+    type: 'cubeTexture',
+    path: [
+      'textures/environmentMap/px.jpb',
+      'textures/environmentMap/nx.jpb',
+      'textures/environmentMap/py.jpb',
+      'textures/environmentMap/ny.jpb',
+      'textures/environmentMap/pz.jpb',
+      'textures/environmentMap/nz.jpx'
+    ]
+  }
+]
