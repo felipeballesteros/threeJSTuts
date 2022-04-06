cf46770aa160c9bda3db33d6513966f658a26868
Felipe Ballesteros <felipeballesteros@msn.com>
2022-04-06 08:26:31 -0400

    finishing lesson 26

--- lesson27 - Shaders/src/shaders/test/vertex.glsl
+++ lesson27 - Shaders/src/shaders/test/vertex.glsl
@@ -1,13 +1,8 @@
-uniform mat4 projectionMatrix;
-uniform mat4 viewMatrix;
-uniform mat4 modelMatrix;
 uniform vec2 uFrequency;
 uniform float uTime;
 
-attribute vec3 position;
-attribute vec2 uv;
-
 varying vec2 vUv;
+varying float vElevation;
 
 void main(){
 
@@ -16,9 +11,11 @@ void main(){
     // Breakdown:
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+
     // Creating the wave here:
-    modelPosition.z += sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+    modelPosition.z += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
@@ -26,5 +23,6 @@ void main(){
     gl_Position = projectedPosition;
 
     vUv = uv;
+    vElevation = elevation;
 
 }
