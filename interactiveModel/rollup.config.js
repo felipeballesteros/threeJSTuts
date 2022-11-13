b25238ac7d3585ac47fd0fdb823d17145069e60c
Felipe Ballesteros <felipeballesteros@msn.com>
2022-11-13 12:56:30 -0500

    adding model, adding initial animation

--- interactiveModel/rollup.config.js
+++ interactiveModel/rollup.config.js
@@ -1,7 +1,7 @@
 import livereload from 'rollup-plugin-livereload'
 import serve from 'rollup-plugin-serve'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   entry: 'main.js',
