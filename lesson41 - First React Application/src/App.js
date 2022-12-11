780835fde4f0efd48edddb1d771ad58b4f7b3f2e
Felipe Ballesteros <felipeballesteros@msn.com>
2022-12-11 15:13:05 -0500

    lesson41 adding component state, useEffect

--- lesson41 - First React Application/src/App.js
+++ lesson41 - First React Application/src/App.js
@@ -1,10 +1,18 @@
 import Clicker from "./Clicker"
-
+import { useState } from 'react'
 
 export default function App(){
+
+  const [ hasClicker, setHasClicker ] = useState(true)
+
+  const toggleClickerClick = () => {
+    setHasClicker(!hasClicker)
+  }
+
   return (
     <>
-      <Clicker />
+      <button onClick={ toggleClickerClick }>{ hasClicker ? 'Hide' : 'Show' } Clicker</button>
+      { hasClicker && <Clicker /> }
     </>
   )
 }
--- lesson41 - First React Application/src/Clicker.js
+++ lesson41 - First React Application/src/Clicker.js
@@ -1,8 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Clicker() {
 
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(parseInt(localStorage.getItem('count') ?? 0))
+
+  // First Time Render
+  useEffect(() => {
+    console.log('component created')
+
+    // Function executed when component is being destroyed
+    return () => {
+      console.log('component destroyed')
+      localStorage.removeItem('count')
+    }
+  }, [])
+
+  // Any other re-render besides the first one
+  useEffect(() => {
+    localStorage.setItem('count', count)
+  }, [ count ])
   
   const buttonClick = () => {
     setCount( count + 1 )
