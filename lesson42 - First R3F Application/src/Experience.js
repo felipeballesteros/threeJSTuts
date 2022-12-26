c83aecd5cfac64b18f3bbc9efe56fd3fa73adf56
Felipe Ballesteros <felipeballesteros@msn.com>
2022-12-25 20:16:35 -0500

    small sample of useFrame from R3F

--- lesson42 - First R3F Application/src/Experience.js
+++ lesson42 - First R3F Application/src/Experience.js
@@ -1,10 +1,18 @@
+import { useFrame } from "@react-three/fiber"
+import { useRef } from "react"
+
 export default function Experience() {
+
+  const cubeRef = useRef()
+  useFrame( ( state, delta ) => {
+    cubeRef.current.rotation.y += delta
+  })
 
   return (
   <>
-    <mesh position-x={1}>
+    <mesh position-x={1} ref={cubeRef}>
         <boxGeometry scale={ 1.5 } />
-        <meshBasicMaterial color={'mediumPurple'} />
+        <meshBasicMaterial color={'mediumPurple'} wireframe={true} />
     </mesh>
 
     <mesh position-x={-1}>
