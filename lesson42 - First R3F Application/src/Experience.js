6b759430739f6e51c259cd42bd0c001ade8c934b
Felipe Ballesteros <felipeballesteros@msn.com>
2022-12-25 19:04:33 -0500

    created a basic scene with R3F

--- lesson42 - First R3F Application/src/Experience.js
+++ lesson42 - First R3F Application/src/Experience.js
@@ -2,9 +2,19 @@ export default function Experience() {
 
   return (
   <>
-    <mesh>
-        <torusKnotGeometry />
-        <meshNormalMaterial />
+    <mesh position-x={1}>
+        <boxGeometry scale={ 1.5 } />
+        <meshBasicMaterial color={'mediumPurple'} />
+    </mesh>
+
+    <mesh position-x={-1}>
+        <sphereGeometry args={ [1, 20, 20] } />
+        <meshBasicMaterial color={'orange'} wireframe={true} />
+    </mesh>
+
+    <mesh rotation-x={Math.PI * -0.5} position-y={-1}>
+        <planeGeometry args={ [4,4] } />
+        <meshBasicMaterial color={'greenyellow'} />
     </mesh>
   </>
   )
