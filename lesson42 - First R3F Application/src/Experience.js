090394a2d4e2e6e1172592234e5b9da1dde18487
Felipe Ballesteros <felipeballesteros@msn.com>
2022-12-28 08:58:22 -0500

    Add example of use of useThree hook

--- lesson42 - First R3F Application/src/Experience.js
+++ lesson42 - First R3F Application/src/Experience.js
@@ -1,15 +1,24 @@
-import { useFrame } from "@react-three/fiber"
+import { useFrame, extend, useThree } from "@react-three/fiber"
 import { useRef } from "react"
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+extend({ OrbitControls })
 
 export default function Experience() {
 
   const cubeRef = useRef()
+
+  // Hook that gets ThreeJS global values only once
+  const { camera, gl } = useThree()
+
   useFrame( ( state, delta ) => {
     cubeRef.current.rotation.y += delta
   })
 
   return (
   <>
+    <orbitControls args={[ camera, gl.domElement ]}/>
+
     <mesh position-x={1} ref={cubeRef}>
         <boxGeometry scale={ 1.5 } />
         <meshBasicMaterial color={'mediumPurple'} wireframe={true} />
