73dfce0b9c4019f59895c43105ce61829f314a94
Felipe Ballesteros <felipeballesteros@msn.com>
2023-01-02 21:54:45 -0500

    created custom geometry, added calculations to useMemo, and set computerVertexNormals after first render with useEffect

--- lesson42 - First R3F Application/src/CustomObject.js
+++ lesson42 - First R3F Application/src/CustomObject.js
@@ -1,21 +1,34 @@
 import * as THREE from 'three'
+import { useMemo, useRef, useEffect } from 'react'
 
 export default function CustomObject() {
-  const verticesCount = 10 * 3
-  const positions = new Float32Array(verticesCount * 3)
 
-  for( let i = 0; i < verticesCount * 3; i++){
-    positions[i] = (Math.random() - 0.5) * 3
-  }
+  const geometryRef = useRef()
+
+  const verticesCount = 10 * 3
+
+  const calculatedPositions = useMemo( () => {
+    const positions = new Float32Array(verticesCount * 3)
+
+    for( let i = 0; i < verticesCount * 3; i++){
+      positions[i] = (Math.random() - 0.5) * 3
+    }
+
+    return positions
+  }, [])
+
+  useEffect( () => {
+    geometryRef.current.computeVertexNormals()
+  }, [])
 
   return (
     <mesh>
-      <bufferGeometry>
+      <bufferGeometry ref={ geometryRef }>
         <bufferAttribute 
           attach='attributes-position'
           count={ verticesCount }
           itemSize={ 3 }
-          array={ positions }
+          array={ calculatedPositions }
         />
       </bufferGeometry>
       <meshBasicMaterial color="red" side={ THREE.DoubleSide } />
