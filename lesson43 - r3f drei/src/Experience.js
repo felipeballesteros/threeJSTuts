712567139482881ab003c87d5d418b614926d109
Felipe Ballesteros <felipeballesteros@msn.com>
2023-01-06 08:51:26 -0500

    fixed gizmo issues, set transformControls outside of cube

--- lesson43 - r3f drei/src/Experience.js
+++ lesson43 - r3f drei/src/Experience.js
@@ -1,10 +1,13 @@
 import { TransformControls, OrbitControls } from '@react-three/drei'
+import { useRef } from 'react'
 
-export default function Experience()
-{
+export default function Experience(){
+
+    const cube = useRef()
     return <>
 
-        <OrbitControls />
+        <OrbitControls makeDefault />
+
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
@@ -13,12 +16,11 @@ export default function Experience()
             <meshStandardMaterial color="orange" />
         </mesh>
 
-        <TransformControls position-x={ 2 }>
-            <mesh scale={ 1.5 }>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-        </TransformControls>
+        <mesh ref={cube} position-x={ 2 } scale={ 1.5 }>
+            <boxGeometry />
+            <meshStandardMaterial color='mediumpurple' />
+        </mesh>
+        <TransformControls object={cube} mode='rotate'/>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
