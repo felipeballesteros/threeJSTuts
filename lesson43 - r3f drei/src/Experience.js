82c718b8c6d3d32e30df09ac603dbf511e2489c7
Felipe Ballesteros <felipeballesteros@msn.com>
2023-01-08 12:26:31 -0500

    Adding Html and PivotControls examples to the scene

--- lesson43 - r3f drei/src/Experience.js
+++ lesson43 - r3f drei/src/Experience.js
@@ -1,9 +1,10 @@
-import { TransformControls, OrbitControls } from '@react-three/drei'
+import { Html, PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience(){
 
     const cube = useRef()
+    const sphere = useRef()
     return <>
 
         <OrbitControls makeDefault />
@@ -11,21 +12,38 @@ export default function Experience(){
         <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+        <PivotControls 
+            anchor={[0, 0, 0]} 
+            depthTest={false}
+            lineWidth={4}
+            axisColors={['#9381ff', '#ff4d6d', '#7ae582']}
+            scale={100}
+            fixed={true} // fixes size on screen regarding of zoom level
+        >
+            <mesh position-x={ - 2 } ref={sphere}>
+                <sphereGeometry/>
+                <meshStandardMaterial color="orange" />
+                <Html 
+                    position={[1, 1, 0]}
+                    wrapperClass='label'
+                    center
+                    distanceFactor={8}
+                    occlude={[cube, sphere]} //Hide HTML if these are in front
+                >
+                    That is a sphere 🤘
+                </Html>
+            </mesh>
+        </PivotControls>
 
         <mesh ref={cube} position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
             <meshStandardMaterial color='mediumpurple' />
         </mesh>
-        <TransformControls object={cube} mode='rotate'/>
+        <TransformControls object={cube} mode='translate'/>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
-
     </>
 }
