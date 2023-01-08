f9b8a3a51d5210719b0cbca788f48e7e0e740fda
Felipe Ballesteros <felipeballesteros@msn.com>
2023-01-08 13:59:36 -0500

    Added example for text rendering

--- lesson43 - r3f drei/src/Experience.js
+++ lesson43 - r3f drei/src/Experience.js
@@ -1,4 +1,4 @@
-import { Html, PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
+import { Text, Html, PivotControls, TransformControls, OrbitControls } from '@react-three/drei'
 import { useRef } from 'react'
 
 export default function Experience(){
@@ -45,5 +45,16 @@ export default function Experience(){
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <Text
+            font= './bangers-v20-latin-regular.woff'
+            fontSize={1}
+            color='salmon'
+            position-y={2}
+            maxWidth={2}
+            textAlign='center'
+        >
+            I LOVE R3F
+        </Text>
     </>
 }
