7921771c6d235269cd43758abbfd25ce469af0cf
Felipe Ballesteros <felipeballesteros@msn.com>
2023-01-22 10:50:50 -0500

    Implementing environmentMap with lighting

--- lesson45 - Environment And Staging With R3F/src/Experience.js
+++ lesson45 - Environment And Staging With R3F/src/Experience.js
@@ -6,6 +6,7 @@ import {
     softShadows,
     OrbitControls,
     Sky,
+    Environment,
     useHelper 
 } from '@react-three/drei'
 import { useRef } from 'react'
@@ -41,12 +42,24 @@ export default function Experience()
         }
     )
 
-    const { sunPosition } = useControls('sky', {
-        sunPosition: { value: [1, 2, 3] }
+    const { envMapIntensity } = useControls('environment map', {
+        envMapIntensity: { value: 3.5, min: 0, max: 12 }
     })
 
+    // const { sunPosition } = useControls('sky', {
+    //     sunPosition: { value: [1, 2, 3] }
+    // })
+
     return <>
+
+        <Environment 
+            background
+            files={'environmentMaps/the_sky_is_on_fire_2k.hdr'}
+
+        />
+        
         <color args={['gray']} attach='background'/>
+        
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
@@ -82,7 +95,7 @@ export default function Experience()
             frames={ 1 }
         />
 
-        <directionalLight
+        {/* <directionalLight
             position={sunPosition } 
             intensity={ 1.5 } 
             ref={directionalLight}
@@ -95,24 +108,25 @@ export default function Experience()
             shadow-camera-right={ 5 }
             shadow-camera-bottom={ - 5 }
             shadow-camera-left={ - 5 }
-        />
-        <ambientLight intensity={ 0.5 } />
+        /> */}
 
-        <Sky sunPosition={sunPosition}/>
+        {/* <ambientLight intensity={ 0.5 } /> */}
+
+        {/* <Sky sunPosition={sunPosition}/> */}
 
         <mesh castShadow position-x={ - 2 }>
             <sphereGeometry />
-            <meshStandardMaterial color="orange" />
+            <meshStandardMaterial color="orange" envMapIntensity={envMapIntensity}/>
         </mesh>
 
         <mesh castShadow ref={ cube } position-x={ 2 } scale={ 1.5 }>
             <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
+            <meshStandardMaterial color="mediumpurple" envMapIntensity={envMapIntensity} />
         </mesh>
 
         <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
+            <meshStandardMaterial color="greenyellow" envMapIntensity={envMapIntensity} />
         </mesh>
 
     </>
