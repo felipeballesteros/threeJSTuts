99ea4d8744e8fee154308f3b066ff8198027cf17
Felipe Ballesteros <felipeballesteros@msn.com>
2023-02-03 08:17:49 -0500

    adding draco loader and model

--- lesson46 - Load Models With R3F/src/Experience.jsx
+++ lesson46 - Load Models With R3F/src/Experience.jsx
@@ -1,8 +1,21 @@
 import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useLoader } from '@react-three/fiber'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
 
 export default function Experience()
 {
+    const model = useLoader( 
+        GLTFLoader, 
+        './FlightHelmet/glTF/FlightHelmet.gltf',
+        (loader) => {
+            const dracoLoader = new DRACOLoader()
+            dracoLoader.setDecoderPath('./draco/')
+            loader.setDRACOLoader(dracoLoader)
+        }
+    )
+    
     return <>
 
         <Perf position="top-left" />
@@ -12,20 +25,12 @@ export default function Experience()
         <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
         <ambientLight intensity={ 0.5 } />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
         <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
             <planeGeometry />
             <meshStandardMaterial color="greenyellow" />
         </mesh>
+
+        <primitive object={model.scene} scale={5} position-y={-1}/>
 
     </>
 }
