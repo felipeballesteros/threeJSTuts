7d75a7718a8cfd45edc6e90e6d3575c5afd80a93
Felipe Ballesteros <felipeballesteros@msn.com>
2023-02-09 09:37:00 -0500

    finalized animations, transitions and cleanup

--- lesson46 - Load Models With R3F/src/Fox.jsx
+++ lesson46 - Load Models With R3F/src/Fox.jsx
@@ -1,10 +1,36 @@
 
 
-import { useGLTF } from '@react-three/drei'
+import { useAnimations,  useGLTF } from '@react-three/drei'
+import { useEffect } from 'react'
+import { useControls } from 'leva'
 
 export default function Fox() {
 
     const fox = useGLTF('./Fox/glTF-Binary/Fox.glb')
+    const animations = useAnimations(fox.animations, fox.scene)
+
+    const { animationName } = useControls({
+        animationName: {
+            options: animations.names
+        }
+    })
+
+    useEffect( () => {
+
+        const action = animations.actions[animationName]
+        action.reset().fadeIn(0.5).play()
+
+        return () => {
+            action.fadeOut(0.5)
+        }
+
+        // setTimeout( () => {
+        //     animations.actions.Walk.play()
+        //     animations.actions.Walk.crossFadeFrom( 
+        //         animations.actions.Run, 1
+        //     )
+        // }, 2000)
+    },[ animationName ])
 
     return <primitive 
         object={fox.scene} 
