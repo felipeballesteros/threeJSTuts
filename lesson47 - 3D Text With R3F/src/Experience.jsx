db3eb3d5ecd92cb535a337ce58751a6ea65d0d54
Felipe Ballesteros <felipeballesteros@msn.com>
2023-02-10 09:00:38 -0500

    lesson47 first commit, adding font and matcap texture

---
+++ lesson47 - 3D Text With R3F/src/Experience.jsx
@@ -0,0 +1,29 @@
+import { useMatcapTexture, Center, OrbitControls, Text3D } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+
+export default function Experience(){
+    const matcapTexture = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <Center>
+            <Text3D 
+                font='./fonts/helvetiker_regular.typeface.json'
+                size={ 0.75 }
+                height={ 0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+            >
+                HELLO R3F
+                <meshNormalMaterial />
+            </Text3D>
+        </Center>
+    </>
+}
