edf5a35ce3614b3ad25d888afddb06858b8270c4
Felipe Ballesteros <felipeballesteros@msn.com>
2023-02-23 08:54:46 -0500

    adding group to create animation on all donuts

--- lesson47 - 3D Text With R3F/src/Experience.jsx
+++ lesson47 - 3D Text With R3F/src/Experience.jsx
@@ -1,14 +1,21 @@
 import { useMatcapTexture, Center, OrbitControls, Text3D } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 import { Perf } from 'r3f-perf'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export default function Experience(){
+
+    const donutsGroup = useRef()
     const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
 
     const [ torusGeometry, setTorusGeometry ] = useState()
     const [ material, setMaterial ] = useState()
 
-
+    useFrame( (state, delta) =>{
+        for( const donut of donutsGroup.current.children){
+            donut.rotation.y += delta * 0.3
+        }
+    })
 
     return <>
 
@@ -37,26 +44,28 @@ export default function Experience(){
             </Text3D>
         </Center>
 
-        {[...Array(100)].map( (value, index) =>
-            <mesh
-                key={index}
-                geometry= { torusGeometry }
-                material= { material }
-                position={[
-                    (Math.random() - 0.5) * 10,
-                    (Math.random() - 0.5) * 10,
-                    (Math.random() - 0.5) * 10,
-                ]}
+        <group ref={ donutsGroup } >
+            {[...Array(100)].map( (value, index) =>
+                <mesh
+                    key={index}
+                    geometry= { torusGeometry }
+                    material= { material }
+                    position={[
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                        (Math.random() - 0.5) * 10,
+                    ]}
 
-                scale={ 0.2 + Math.random() * 0.2 }
+                    scale={ 0.2 + Math.random() * 0.2 }
 
-                rotation={[
-                    Math.random() * Math.PI,
-                    Math.random() * Math.PI,
-                    0
-                ]}
-            />
-        )}
+                    rotation={[
+                        Math.random() * Math.PI,
+                        Math.random() * Math.PI,
+                        0
+                    ]}
+                />
+            )}
+        </group>
 
     </>
 }
