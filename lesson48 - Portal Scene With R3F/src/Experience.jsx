3f60ff1e2d39c8f68dd756248d9ab4ea7d86d95e
Felipe Ballesteros <felipeballesteros@msn.com>
2023-02-24 08:49:44 -0500

    loading model and texture

--- lesson48 - Portal Scene With R3F/src/Experience.jsx
+++ lesson48 - Portal Scene With R3F/src/Experience.jsx
@@ -1,15 +1,23 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, useGLTF, useTexture } from '@react-three/drei'
 
-export default function Experience()
-{
+export default function Experience(){
+
+    const { nodes } = useGLTF('./model/portal.glb')
+
+    const bakedTexture = useTexture('./model/baked.jpg')
+    bakedTexture.flipY = false // flip texture on y axis to make it fit
+    
+
     return <>
+
+        <color args={['#030202']} attach='background'/>
 
         <OrbitControls makeDefault />
 
-        <mesh scale={ 1.5 }>
-            <boxGeometry />
-            <meshNormalMaterial />
+        <mesh geometry={ nodes.baked.geometry }>
+            <meshBasicMaterial map={ bakedTexture } />
         </mesh>
+        
 
     </>
 }
