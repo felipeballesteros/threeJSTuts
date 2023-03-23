31d1207be157c338ff94df7b455a37e70155911f
Felipe Ballesteros <felipeballesteros@msn.com>
2023-03-23 08:53:05 -0400

    add animations and rotation controls

--- lesson51 - Fun And Simple Portfolio With R3F/src/Experience.jsx
+++ lesson51 - Fun And Simple Portfolio With R3F/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { Float, Environment, useGLTF, OrbitControls } from '@react-three/drei'
+import { PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
 
 export default function Experience()
 {
@@ -10,15 +10,22 @@ export default function Experience()
         <Environment preset='city' />
 
         <color args={ ['#241a1a'] } attach='background' />
-
-        <OrbitControls makeDefault />
         
+        <PresentationControls 
+            global
+            rotation={ [ 0.13, 0.1, 0 ] }
+            polar={ [ -0.4, 0.2 ] }
+            azimuth={ [ -1, 0.75 ] }
+            config={ { mass: 2, tension: 400 } }
+            snap={ { mass: 4, tension: 400} }
+        >
         <Float rotationIntensity={0.4}>
             <primitive
                 position-y={ -1.2 }
                 object={ computer.scene } 
             />
         </Float>
+        </PresentationControls>
 
     </>
 }
