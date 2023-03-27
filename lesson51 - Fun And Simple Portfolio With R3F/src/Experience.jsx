8e6d84d6134de65de3ad74593ac3f7eb97392a66
Felipe Ballesteros <felipeballesteros@msn.com>
2023-03-26 21:47:39 -0400

    Adding website iframe and light for screen reflection

--- lesson51 - Fun And Simple Portfolio With R3F/src/Experience.jsx
+++ lesson51 - Fun And Simple Portfolio With R3F/src/Experience.jsx
@@ -1,4 +1,4 @@
-import { ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
+import { Html, ContactShadows, PresentationControls, Float, Environment, useGLTF } from '@react-three/drei'
 
 export default function Experience()
 {
@@ -20,10 +20,28 @@ export default function Experience()
             snap={ { mass: 4, tension: 400} }
         >
         <Float rotationIntensity={0.4}>
+            <rectAreaLight
+                width={ 2.5 }
+                height={ 1.65 }
+                intensity={ 65 }
+                color={ '#ff6900'}
+                rotation={ [ 0.1, Math.PI, 0] }
+                position={ [ 0, 0.55, -1.15 ] }
+            />
             <primitive
                 position-y={ -1.2 }
                 object={ computer.scene } 
-            />
+            >
+                <Html
+                    transform
+                    wrapperClass='htmlScreen'
+                    distanceFactor={ 1.17 }
+                    position={ [ 0, 1.56, -1.4]}
+                    rotation-x= { -0.256}
+                >
+                    <iframe src='https://bruno-simon.com/html/' />
+                </Html>
+            </primitive>
         </Float>
         </PresentationControls>
 
