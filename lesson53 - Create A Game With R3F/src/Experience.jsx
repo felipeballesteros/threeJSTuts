8ad6e18176f72f347453dd2125519080266df034
Felipe Ballesteros <felipeballesteros@msn.com>
2023-04-09 23:06:00 -0400

    setup initial starting block for the floor

--- lesson53 - Create A Game With R3F/src/Experience.jsx
+++ lesson53 - Create A Game With R3F/src/Experience.jsx
@@ -1,4 +1,5 @@
 import { OrbitControls } from '@react-three/drei'
+import { Physics, Debug} from '@react-three/rapier'
 import Lights from './Lights.jsx'
 import Level from './Level.jsx'
 
@@ -9,8 +10,12 @@ export default function Experience()
 
         <OrbitControls makeDefault />
 
-        <Lights />
-        <Level />
+        <Physics>
+            <Debug />
+            <Lights />
+            <Level />
+        </Physics>
+        
 
     </>
 }
